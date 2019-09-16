d114b08ed9fcaa0cdf026a1a54a47cbcc0d762e6
mui <mui@one.com>
2019-09-16 07:05:09 +0400

    File io tests

---
+++ src/io/readfile/readfile.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	const SRCPATH = "/home/mui/one/data/webshop-data-20190813/incoming/webshop1.cst.webpod9-cph3.one.com/order_events.json"
+	const DSTPATH = "/home/mui/one/data/webshop-data-20190813/incoming/webshop1.cst.webpod9-cph3.one.com/outfile"
+
+	fmt.Printf("Source file: \n%s\n", SRCPATH)
+	src, err := os.OpenFile(SRCPATH, os.O_RDONLY, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer src.Close()
+
+	dst, err := os.OpenFile(DSTPATH, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Destination file: \n%s\n", DSTPATH)
+	defer dst.Close()
+
+	buf := make([]byte, 1024*1000)
+
+	n, err := src.Read(buf)
+
+	for err == nil && n > 0 {
+		dst.Write(buf)
+		n, err = src.Read(buf)
+	}
+}
